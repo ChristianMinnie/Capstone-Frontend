57da6400354da19a56ef53361b3008cdd36e247c
ChristianMinnie <christian.minnie@gmail.com>
2021-11-01 12:39:09 -0500

    Adds signup, login, and logout pages/actions

---
+++ src/views/TutorialsIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tutorials-index">
+    <h1>All Tutorials</h1>
+    <div v-for="tutorial in tutorials" v-bind:key="tutorial.id">
+      {{ tutorial }}
+      <h2>{{ tutorial.description }}</h2>
+      <a v-bind:href="tutorial.hyperlink">go to tutorial</a>
+      <!-- <img v-bind:src="tutorial.url" v-bind:alt="tutorial.name" />
+      <p>Width: {{ tutorial.width }}</p>
+      <p>Height: {{ tutorial.height }}</p> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      tutorials: [],
+    };
+  },
+  created: function () {
+    this.indexTutorials();
+  },
+  methods: {
+    indexTutorials: function () {
+      axios.get("/tutorials").then((response) => {
+        console.log("tutorials index", response);
+        this.tutorials = response.data;
+      });
+    },
+  },
+};
+</script>
