70ad797a65334530bf7928b8dfcabf7e317e6026
ChristianMinnie <christian.minnie@gmail.com>
2021-11-02 12:37:11 -0500

    Adds indexTopics and indexLanguages to TutorialsNew

--- src/views/TutorialsNew.vue
+++ src/views/TutorialsNew.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="tutorials-new">
     <h1>All possible topics</h1>
+
+    <div v-for="topic in topics" v-bind:key="`topic-${topic.id}`">
+      <input type="checkbox" :id="topic.name" :value="topic.id" v-model="checkedTopics" />
+      <label :for="topic.name">{{ topic.name }}</label>
+    </div>
+    <span>Checked topics: {{ checkedTopics }}</span>
+
     <h1>All possible languages</h1>
+    <div v-for="language in languages" v-bind:key="`language-${language.id}`">
+      <input type="checkbox" :id="language.name" :value="language.id" v-model="checkedLanguages" />
+      <label :for="language.name">{{ language.name }}</label>
+    </div>
+    <span>Checked languages: {{ checkedLanguages }}</span>
+
     <h1>New Tutorial</h1>
     <form v-on:submit.prevent="createTutorial()">
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
-      <!-- Name:
-      <input type="text" v-model="newTutorialParams.name" /> -->
       Description:
       <input type="text" v-model="newTutorialParams.description" />
       URL:
@@ -25,12 +36,20 @@ export default {
   data: function () {
     return {
       newTutorialParams: {},
+      topics: [],
+      languages: [],
       errors: [],
+      checkedTopics: [],
+      checkedLanguages: [],
     };
   },
-  created: function () {},
+  created: function () {
+    this.indexTopics();
+    this.indexLanguages();
+  },
   methods: {
     createTutorial: function () {
+      this.newTutorialParams.topic_ids = this.checkedTopics;
       axios
         .post("/tutorials", this.newTutorialParams)
         .then((response) => {
@@ -41,6 +60,18 @@ export default {
           console.log("tutorials create error", error.response);
           this.errors = error.response.data.errors;
         });
+    },
+    indexTopics: function () {
+      axios.get("/topics").then((response) => {
+        console.log("topics index", response);
+        this.topics = response.data;
+      });
+    },
+    indexLanguages: function () {
+      axios.get("/languages").then((response) => {
+        console.log("languages index", response);
+        this.languages = response.data;
+      });
     },
   },
 };
