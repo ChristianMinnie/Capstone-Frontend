f39ae262e77551c906304bd1109f54fb9a599243
ChristianMinnie <christian.minnie@gmail.com>
2021-11-08 15:14:39 -0600

    Adds tutorials cards

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,17 @@
             <li><a class="nav-link scrollto" href="/">Home</a></li>
             <li><a class="nav-link scrollto" href="/about">About</a></li>
             <li><a class="nav-link scrollto" href="/tutorials">Tutorials</a></li>
+            <li><a class="nav-link scrollto" href="/tutorials/new">New Tutorial</a></li>
+            <li><a class="nav-link scrollto" href="/topics">Topics</a></li>
+            <li><a class="nav-link scrollto" href="/languages">Languages</a></li>
+            <li v-if="!isLoggedin()"><a class="nav-link scrollto" href="/signup">Signup</a></li>
+            <li v-if="!isLoggedin()"><a class="nav-link scrollto" href="/login">Login</a></li>
+            <li v-if="isLoggedin()"><a class="nav-link scrollto" href="/logout">Logout</a></li>
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
+          <div class="container">
+            <div v-if="flashMessage" class="alert alert-success" v-on:click="flashMessage = ''">{{ flashMessage }}</div>
+          </div>
         </nav>
         <!-- .navbar -->
       </div>
@@ -75,13 +84,117 @@
       </div>
     </section>
     <!-- End Hero -->
-    <div class="container">
+    <main id="main">
       <router-view />
-    </div>
+    </main>
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">About us</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Requests</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Forum</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>Explore by Topic</h4>
+              <ul>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Web Design</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Product Management</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Marketing</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i>
+                  <a href="#">Graphic Design</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-contact">
+              <h4>Contact Me</h4>
+              <p>
+                <strong>Email:</strong>
+                christian.minnie@gmail.com
+                <br />
+                <strong>GitHub:</strong>
+                https://github.com/ChristianMinnie
+                <strong>LinkedIn:</strong>
+                www.linkedin.com/in/christian-minnie
+                <br />
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="github"><i class="bx bxl-github"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-info">
+              <h3>About Coders Compendium</h3>
+              <p>
+                Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies
+                darta donna mare fermentum iaculis eu non diam phasellus.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright
+          <strong><span>Anyar</span></strong>
+          . All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
+          Designed by
+          <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer -->
   </div>
 </template>
 
-<style></style>
+<style>
+#hero {
+  height: 500px;
+}
+</style>
 
 <script>
 export default {
--- src/views/TutorialsIndex.vue
+++ src/views/TutorialsIndex.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="tutorials-index">
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>Team</h2>
+          <p>
+            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit
+            in iste officiis commodi quidem hic quas.
+          </p>
+        </div>
+
+        <div class="row">
+          <div
+            v-for="tutorial in filterBy(tutorials, filter)"
+            v-bind:key="tutorial.id"
+            class="col-lg-6"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
+            <div class="member d-flex align-items-start mb-2">
+              <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt="" /></div>
+              <div class="member-info">
+                <h4>{{ tutorial.description }}</h4>
+                <span>
+                  <a v-bind:href="tutorial.hyperlink" target="_blank">{{ tutorial.hyperlink }}</a>
+                </span>
+                <p>Explicabo voluptatem mollitia et repellat qui dolorum quasi</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""><i class="ri-linkedin-box-fill"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Team Section -->
+
     <!-- <div v-for="topic in tutorial.topics" v-bind:key="`topic-${topic.id}`">
       <p>Topic: {{ topic }}</p>
     </div>
